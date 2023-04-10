8a4c610da389029e1340225b7e71975981b4c82e
Вячеслав <slavakyrlan00@mail.ru>
2023-04-10 21:22:41 +0300

    Добавления md
    Добавил описание каждого файла

--- Code/TaskPlanner/main.py
+++ Code/TaskPlanner/main.py
@@ -1,19 +1,13 @@
-# This Python file uses the following encoding: utf-8
 import sys
-from pathlib import Path
 
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
-
+#from pathlib import Path
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
 
     engine = QQmlApplicationEngine()
-    qml_file = Path(__file__).resolve().parent / "main.qml"
-    engine.load("main.qml")
+    engine.load("main.qml")   # файл с кодом QML основного окна
 
-
-    if not engine.rootObjects():
-        sys.exit(-1)
-    sys.exit(app.exec())
+    sys.exit(app.exec()) # запустить цикл события
