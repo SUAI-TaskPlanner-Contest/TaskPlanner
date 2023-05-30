a46abeba28836823b1b5dc4f1ea2442807feae43
astronik00 <astronik00@gmail.com>
2023-05-30 19:16:16 +0300

    Fixed naming

--- Code/exceptions/custom_exceptions.py
+++ Code/exceptions/custom_exceptions.py
@@ -1,8 +1,8 @@
 from typing import Tuple
-from Code.handlers.warning_window_handler import ErrorWindowHandler
+from Code.handlers.warning_window_handler import WarningWindowHandler
 
 
 class CustomException(Exception):
     def __init__(self, error_info: Tuple[str, str, str, str]) -> None:
         super().__init__(error_info[1])
-        self.error_info = ErrorWindowHandler(*error_info)
+        self.error_info = WarningWindowHandler(*error_info)
