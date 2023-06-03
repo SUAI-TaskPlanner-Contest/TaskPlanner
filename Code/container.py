55b02effc958249bfb7315b3bf7c0c285cd3a888
astronik00 <astronik00@gmail.com>
2023-06-03 23:28:14 +0300

    added container.py

--- Code/container.py
+++ Code/container.py
@@ -6,9 +6,37 @@ from Code.repositories.server_repo import ServerRepository
 from Code.repositories.task_repo import TaskRepository
 from Code.services import ServerService, TaskService
 
+
+class Container:
+    dict = {}
+    setters = ['server_service', 'task_service', 'caldav_service']
+
+    def __init__(self, **kwargs):
+        self.dict = kwargs
+
+    @staticmethod
+    def get(name):
+        return Container.dict[name]
+
+    @staticmethod
+    def set(name, value):
+        if name in Container.setters:
+            Container.dict[name] = value
+
+    def __repr__(self):
+        return f"Container(id={self.key}, {self.subkey})"
+
+
+# def init_container():
+#     global container
+#     container.set('server_service', ServerService(ServerRepository(session)))
+#     container.set('task_service', TaskService(TaskRepository(session)))
+
+
 engine = create_engine('sqlite:///./database/taskplanner.db', echo=False)  # path to .db
 Base.metadata.create_all(engine)  # create tables
 session = sessionmaker(bind=engine)()  # create transaction
 
-server_service = ServerService(ServerRepository(session))
-task_service = TaskService(TaskRepository(session))
+container = Container
+container.set('server_service', ServerService(ServerRepository(session)))
+container.set('task_service', TaskService(TaskRepository(session)))
--- Code/main.py
+++ Code/main.py
@@ -2,12 +2,12 @@ import os
 import sys
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
-from Code.container import server_service
+from Code.container import container
 from Code.handlers.settings_window_handler import SettingsWindow
 
 
 if __name__ == '__main__':
-    settings = SettingsWindow(server_service)
+    settings = SettingsWindow(container.get('server_service'))
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     engine.rootContext().setContextProperty("settings", settings)
